75e95eb400abbc07073261661b5357a74cc22a7d
ventsislav mladenov <ventsislav.mladenov@gmail.com>
2013-09-25 10:57:57 +0300

    Move Build Output to Monodevelop Addins Directory

--- MonoDevelop.VersionControl.TFS/TFSClient.cs
+++ MonoDevelop.VersionControl.TFS/TFSClient.cs
@@ -2,11 +2,33 @@ using System;
 
 namespace MonoDevelop.VersionControl.TFS
 {
-    public class TFSClient
+    public class TFSClient : VersionControlSystem
     {
         public TFSClient()
         {
         }
+
+        #region implemented abstract members of VersionControlSystem
+
+        protected override Repository OnCreateRepositoryInstance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IRepositoryEditor CreateRepositoryEditor(Repository repo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
     }
 }
 
