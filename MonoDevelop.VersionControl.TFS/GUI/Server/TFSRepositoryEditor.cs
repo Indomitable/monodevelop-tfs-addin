ff50ff3c0d996b06f5c5570302adcf92a29c727a
Indomitable <ventsislav.mladenov@gmail.com>
2013-12-02 13:51:07 +0200

    1. On second thoughts - disable MonoDevelop Checkout.

--- MonoDevelop.VersionControl.TFS/GUI/Server/TFSRepositoryEditor.cs
+++ MonoDevelop.VersionControl.TFS/GUI/Server/TFSRepositoryEditor.cs
@@ -32,48 +32,51 @@ namespace MonoDevelop.VersionControl.TFS.GUI.Server
 {
     public class TFSRepositoryEditor : IRepositoryEditor
     {
-        readonly AddServerWidget widget;
-        readonly TFSRepository repo;
-
+        //        readonly AddServerWidget widget;
+        //        readonly TFSRepository repo;
         public TFSRepositoryEditor(TFSRepository repo)
         {
-            this.repo = repo;
-            widget = new AddServerWidget();
+//            this.repo = repo;
+//            widget = new AddServerWidget();
         }
 
         #region IRepositoryEditor implementation
 
         public bool Validate()
         {
-            var res = !string.IsNullOrEmpty(widget.ServerName) && widget.ServerUrl != null;
-            if (res)
-            {
-                this.repo.Name = widget.ServerName;
-                using (var credentialsDialog = new CredentialsDialog())
-                {
-                    if (credentialsDialog.Run(widget.ParentWindow) == Command.Ok && credentialsDialog.Credentials != null)
-                    {
-                        var server = new TeamFoundationServer(widget.ServerUrl, widget.ServerName, 
-                                         credentialsDialog.Credentials.Domain, 
-                                         credentialsDialog.Credentials.UserName, 
-                                         credentialsDialog.Credentials.Password, 
-                                         true);
-                        server.LoadProjectConnections();
-                        if (server.ProjectCollections.Count != 1)
-                            return false;
-                        this.repo.VersionControlService = server.ProjectCollections[0].GetService<RepositoryService>();
-                        return true;
-                    }
-                }
-            }
             return false;
+//            var res = !string.IsNullOrEmpty(widget.ServerName) && widget.ServerUrl != null;
+//            if (res)
+//            {
+//                this.repo.Name = widget.ServerName;
+//                using (var credentialsDialog = new CredentialsDialog())
+//                {
+//                    if (credentialsDialog.Run(widget.ParentWindow) == Command.Ok && credentialsDialog.Credentials != null)
+//                    {
+//                        var server = new TeamFoundationServer(widget.ServerUrl, widget.ServerName, 
+//                                         credentialsDialog.Credentials.Domain, 
+//                                         credentialsDialog.Credentials.UserName, 
+//                                         credentialsDialog.Credentials.Password, 
+//                                         true);
+//                        server.LoadProjectConnections();
+//                        if (server.ProjectCollections.Count != 1)
+//                            return false;
+//                        this.repo.VersionControlService = server.ProjectCollections[0].GetService<RepositoryService>();
+//                        return true;
+//                    }
+//                }
+//            }
+//            return false;
         }
 
         public Gtk.Widget Widget
         {
             get
             {
-                return (Gtk.Widget)Toolkit.CurrentEngine.GetNativeWidget(widget);
+                Gtk.VBox vbox = new Gtk.VBox();
+                vbox.PackStart(new Gtk.Label("Plase Use Source Control Explorer for Checkout"));
+                vbox.ShowAll();
+                return vbox; //(Gtk.Widget)Toolkit.CurrentEngine.GetNativeWidget(widget);
             }
         }
 
