be7e1c913ae0cfdc83056ff8469f37f16719a58c
Ventsislav Mladenov <ventsislav.mladenov@gmail.com>
2015-03-13 00:14:13 +0200

    1. Rename MonoDevelop.VersionControl.TFS.addin.xml to Manifest.addin.xml

--- MonoDevelop.VersionControl.TFS/Properties/AddinInfo.cs
+++ MonoDevelop.VersionControl.TFS/Properties/AddinInfo.cs
@@ -1,14 +1,14 @@
 ﻿using Mono.Addins;
 
-[assembly:Addin (
+[assembly:Addin(
     "VersionControl.TFS", Namespace = "MonoDevelop", 
     Version = "1.7", Category = "Version Control")]
 
-[assembly:AddinName ("TFS support")]
-[assembly:AddinDescription ("TFS support for the Version Control Add-in")]
-[assembly:AddinUrl ("http://indomitable.github.io/monodevelop-tfs-addin")]
-[assembly:AddinAuthor ("Ventsislav Mladenov <ventsislav.mladenov@gmail.com>")]
+[assembly:AddinName("TFS support")]
+[assembly:AddinDescription("TFS support for the Version Control Add-in")]
+[assembly:AddinUrl("http://indomitable.github.io/monodevelop-tfs-addin")]
+[assembly:AddinAuthor("Ventsislav Mladenov <ventsislav.mladenov@gmail.com>")]
 
-[assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
-[assembly:AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
-[assembly:AddinDependency ("VersionControl", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency("Core", "5.7")]
+[assembly:AddinDependency("Ide", "5.7")]
+[assembly:AddinDependency("VersionControl", "5.7")]
