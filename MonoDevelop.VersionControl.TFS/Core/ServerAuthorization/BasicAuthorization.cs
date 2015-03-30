60dc500fcb28fe6bb4e0dce60921a516f656df6a
Indomitable <ventsislav.mladenov@gmail.com>
2015-03-30 17:29:17 +0300

    1. Add Basic and Ntlm Authorization classes

---
+++ MonoDevelop.VersionControl.TFS/Core/ServerAuthorization/BasicAuthorization.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MonoDevelop.VersionControl.TFS.Core.ServerAuthorization
+{
+    sealed class BasicAuthorization : UserPasswordAuthorization, IServerAuthorization
+    {
+        private BasicAuthorization()
+        {
+            
+        }
+
+        public void Authorize(HttpWebRequest request)
+        {
+            request.Headers.Add(HttpRequestHeader.Authorization, this.AuthorizationHeader);
+        }
+
+        public void Authorize(WebClient client)
+        {
+            client.Headers.Add(HttpRequestHeader.Authorization, this.AuthorizationHeader);
+        }
+
+        public static BasicAuthorization FromConfigXml(XElement element, Uri serverUri)
+        {
+            var auth = new BasicAuthorization();
+            auth.ReadConfig(element, serverUri);
+            return auth;
+        }
+
+        public XElement ToConfigXml()
+        {
+            var element = new XElement("Basic",
+                            new XAttribute("UserName", UserName));
+            if (SavePassword)
+            {
+                element.Add(new XAttribute("Password", Password));
+            }
+            return element;
+        }
+
+        private string AuthorizationHeader
+        {
+            get
+            {
+                var credentialBuffer = Encoding.UTF8.GetBytes(this.UserName + ":" + this.Password);
+                return "Basic " + Convert.ToBase64String(credentialBuffer);
+            }
+        }
+    }
+}
---
+++ MonoDevelop.VersionControl.TFS/Core/ServerAuthorization/NtlmAuthorization.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Xml.Linq;
+using MonoDevelop.VersionControl.TFS.Helpers;
+
+namespace MonoDevelop.VersionControl.TFS.Core.ServerAuthorization
+{
+    sealed class NtlmAuthorization : UserPasswordAuthorization, IServerAuthorization
+    {
+        private NtlmAuthorization()
+        {
+            
+        }
+
+        public void Authorize(HttpWebRequest request)
+        {
+            request.Credentials = new NetworkCredential(UserName, Password, Domain);
+        }
+
+        public void Authorize(WebClient client)
+        {
+            client.Credentials = new NetworkCredential(UserName, Password, Domain);
+        }
+
+        public string Domain { get; private set; }
+
+        public static NtlmAuthorization FromConfigXml(XElement element, Uri serverUri)
+        {
+            var auth = new NtlmAuthorization();
+            auth.ReadConfig(element, serverUri);
+            auth.Domain = element.GetAttributeValue("Domain");
+            return auth;
+        }
+
+        public XElement ToConfigXml()
+        {
+            var element = new XElement("Ntlm", 
+                            new XAttribute("UserName", UserName),
+                            new XAttribute("Domain", Domain));
+            if (SavePassword)
+            {
+                element.Add(new XAttribute("Password", Password));
+            }
+            return element;
+        }
+    }
+}
